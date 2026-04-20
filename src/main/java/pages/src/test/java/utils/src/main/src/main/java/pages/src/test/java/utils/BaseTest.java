50b601b43b63f3dd81e8079940de36fee0846c10
sangeetha-siva <sangeethasiva2804@gmail.com>
2026-04-20 11:46:29 +0530

    Create BaseTest.java

---
+++ src/main/java/pages/src/test/java/utils/src/main/src/main/java/pages/src/test/java/utils/BaseTest.java
@@ -0,0 +1,34 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com");
+        driver.manage().timeouts().implicitlyWait(
+            java.time.Duration.ofSeconds(10)
+        );
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
